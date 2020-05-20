1a05b3ffe7ae743c30ee1323759b872e1fa0030c
EvannDev <evann.de-bailliencourt@isen.yncrea.fr>
2020-05-20 14:05:33 +0200

    clean before merge master

--- microservice-video/src/main/java/com/livefox/video/repository/VideoRepository.java
+++
@@ -1,8 +0,0 @@
-package com.livefox.video.repository;
-
-import com.livefox.video.model.Video;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface VideoRepository extends JpaRepository<Video, Integer> {
-    Video findById(int id);
-}
